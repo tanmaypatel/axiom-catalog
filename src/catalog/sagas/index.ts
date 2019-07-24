eb5c3cd915c4e16674b735ee4888cbc725aee2a3
Tanmay Patel <me@tanmaypatel.com>
2019-07-24 21:48:18 +0400

    Fixed Saga Interaction

--- src/catalog/sagas/index.ts
+++ src/catalog/sagas/index.ts
@@ -3,9 +3,12 @@ import { call, put, takeEvery, takeLatest } from 'redux-saga/effects';
 
 import { actions, loadCatalogStart, loadCatalogSuccess, loadCatalogError } from '../actions';
 
+const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
 function* loadCatalog(action: FSA<string, any>): IterableIterator<any> {
     try {
         yield put(loadCatalogStart(action.payload.offset, action.payload.limit));
+        yield delay(1000);
         yield put(loadCatalogSuccess([], action.payload.offset, action.payload.limit));
     } catch (error) {
         yield put(loadCatalogError(error, action.payload.offset, action.payload.limit));
@@ -13,7 +16,7 @@ function* loadCatalog(action: FSA<string, any>): IterableIterator<any> {
 }
 
 function* catalogSaga(): IterableIterator<any> {
-    takeLatest(actions.LOAD_CATALOG, loadCatalog);
+    yield takeLatest(actions.LOAD_CATALOG, loadCatalog);
 }
 
 export default catalogSaga;
